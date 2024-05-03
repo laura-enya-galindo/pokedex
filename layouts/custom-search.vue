d727b9c18edaff0253cb8028a25dcd38212d8d28
laura-enya-galindo <91256477+laura-enya-galindo@users.noreply.github.com>
2024-05-03 15:32:42 +0200

    Ajout de "custom-search.vue" pour que le bouton sur la page de recherche mène vers la home et pas vers la page de recherche

---
+++ layouts/custom-search.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    const goToHome = () => {
+        router.push(`/`);
+    };
+
+</script>
+
+<template>
+    <div>
+        <button @click="goToHome()"> Home </button>
+        <slot/>
+    </div>
+</template>
