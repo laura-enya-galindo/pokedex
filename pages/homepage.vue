d12e87bef4bfc6623579a8351eaaeb7beb2d21b1
laura-enya-galindo <91256477+laura-enya-galindo@users.noreply.github.com>
2024-05-03 10:49:36 +0200

    Début page d'accueil

--- pages/homepage.vue
+++ pages/homepage.vue
@@ -1,12 +1,32 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+
+    let search: string = "";
+    const pokemonList = reactive<string[]>([]);
+
+    function searchPokemon() {
+        if (search.trim() !== "") {
+            // Recherche d'un pokémon
+        }
+        search = "";
+    }
+
+    function goToInformation(index: number) {
+
+    }
+</script>
+
 <template>
     <div>
-        <Header>
-            <template v-slot:header>
-                About
-            </template>
-            <template v-slot:description>
-                This is me !
-            </template>
-        </Header>
+        <input type="text" v-model="search">
+        <button @click="searchPokemon">Search Pokemon</button>
+
+        <ul>
+            <li v-for="(pokemon, i) in pokemonList">{{ pokemon }} <button @click="goToInformation(i)"> More information </button></li>
+        </ul>
+
+        <!--Créer condition pour afficher le résultat de la recherche-->
+        <p v-if="pokemonList.length >= 10">Tu as 10 tâches à faire !</p>
+
     </div>
 </template>
