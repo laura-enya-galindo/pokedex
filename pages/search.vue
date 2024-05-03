4d4261e30eba3885471728c056de404dd7a5cffb
Lauryne <104195191+lauryne921@users.noreply.github.com>
2024-05-03 11:07:29 +0000

    search pokemon and redirect to the single page

---
+++ pages/search.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { data: pokemon } = await useFetch('https://pokeapi.co/api/v2/pokemon/')
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+let searchPokemon = ref("");
+
+const goToPokemon = () => {
+    router.push(`/single/${searchPokemon.value}`);
+};
+
+
+</script>
+
+<template>
+    <input type="search" name="pokemon-search" id="pokemon-search" v-model="searchPokemon" v-on:keyup.enter="goToPokemon">
+</template>
